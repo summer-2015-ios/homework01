ef4b1d9f7b43f08f624be00f3b44a5bfed006581
student <student@ccibio104-206.uncc.edu>
2015-07-12 12:56:27 -0400

    Initial Commit

---
+++ Homework01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homework01
+//
+//  Created by student on 7/12/15.
+//  Copyright (c) 2015 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
