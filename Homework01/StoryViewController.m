05cfb3ed33fab1f22c32d4f30554b9adc9607c9a
student <student@ccibio104-206.uncc.edu>
2015-07-13 17:40:53 -0400

    player left

--- Homework01/StoryViewController.m
+++ Homework01/StoryViewController.m
@@ -8,6 +8,7 @@
 
 #import "StoryViewController.h"
 #import "WebViewViewController.h"
+@import AVFoundation;
 
 @interface StoryViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *titleView;
@@ -15,7 +16,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *dateAiredView;
 @property (weak, nonatomic) IBOutlet UILabel *durationView;
 @property (weak, nonatomic) IBOutlet UILabel *teaserView;
-
+@property (strong, nonatomic) NSString* audioPlayableUrl;
+@property (strong, nonatomic) AVPlayer *player;
 @end
 
 @implementation StoryViewController
@@ -27,6 +29,7 @@
     self.dateAiredView.text = self.dateAired;
     self.durationView.text = [NSString stringWithFormat:@"%ld", self.duration ];
     self.teaserView.text = self.teaser;
+    [self fetchMp3Stream];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,5 +43,47 @@
 }
 -(IBAction)backFromWebView:(UIStoryboardSegue* )segue{
     NSLog(@"Back from web view");
+}
+
+-(void) fetchMp3Stream{
+    if(!self.audio){
+        NSLog(@"no audio");
+        return;
+    }
+    NSURLRequest *request  = [NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@", self.audio]]];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    [[session dataTaskWithRequest:request
+                completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                    if(error){
+                        NSLog(@"error : %@", error);
+                        return ;
+                    }
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        self.audioPlayableUrl = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                        NSLog(@"back from url : %@", self.audioPlayableUrl);
+                    });
+                    
+                }] resume];
+}
+- (IBAction)audioClicked:(UIButton *)sender {
+   // NSLog(@"1url = %@\n 2 url= %@",self.audio, self.audioPlayableUrl);
+    self.player = [[AVPlayer alloc] initWithURL:[NSURL URLWithString:self.audioPlayableUrl]];
+//    AVAudioPlayer *player = [[AVAudioPlayer alloc] initWithContentsOfURL:([NSURL URLWithString:self.audioPlayableUrl])
+//                                                                   error:&error];
+//    
+    [self.player play];
+    NSLog(@"Volume %f", self.player.volume);
+}
+- (IBAction)playerRewind:(UIButton *)sender {
+   // self.player
+}
+- (IBAction)playerPlay:(UIButton *)sender {
+}
+- (IBAction)playerForward:(UIButton *)sender {
+}
+- (IBAction)stopPlay:(id)sender {
+    if(self.player){
+        [self.player pause];
+    }
 }
 @end
